a1ba23af03a305bf14fd9cc648c9bc9784b503eb
TilGP <60795390+TilGP@users.noreply.github.com>
2021-12-07 10:02:47 +0100

    Update navbar.js
    fixed navbar going to mobile mode, when clicking links

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,6 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
+    if(getWidth() < 768){
+      const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
@@ -14,4 +15,16 @@ const navSlide = () => {
             link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.3}s`;
         }
     });
+    }
+    
+}
+
+function getWidth() {
+  if (self.innerWidth) {
+    return self.innerWidth;
+  }else if (document.documentElement && document.documentElement.clientWidth){
+    return document.documentElement.clientWidth;
+  }else if(document.body){
+    return document.body;
+  }
 }
