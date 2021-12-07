e91c2533b38d92a95c0f30812540345d96fddfdf
TilGP <60795390+TilGP@users.noreply.github.com>
2021-12-07 10:23:47 +0100

    Update navbar.js
    made navbar only able to go in mobile mode, when with is under 768px

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,21 +1,22 @@
 const navSlide = () => {
-    
   if (getWidth() < 768){
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-
-    //toggle nav
-    nav.classList.toggle('nav-active');
-
-    //links fade
-    navLinks.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation = '';
-        }else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.3}s`;
-        }
-    });
+    if (getWidth() < 768){
+      const burger = document.querySelector('.burger');
+      const nav = document.querySelector('.nav-links');
+      const navLinks = document.querySelectorAll('.nav-links li');
+  
+      //toggle nav
+      nav.classList.toggle('nav-active');
+  
+      //links fade
+      navLinks.forEach((link, index) => {
+          if(link.style.animation){
+              link.style.animation = '';
+          }else{
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.3}s`;
+          }
+      });
+    }
   }
 }
 
